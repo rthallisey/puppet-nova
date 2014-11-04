10ef5544783e961b96aee17ca301fec183cb2ac6
Ryan Hallisey <rhallise@redhat.com>
2014-11-04 13:32:48 -0500

    Fix syntax errors in ironic module

--- manifests/compute/ironic.pp
+++ manifests/compute/ironic.pp
@@ -41,18 +41,18 @@ class nova::compute::ironic (
   $admin_url = 'http://127.0.0.1:35357/v2.0',
   $admin_tenant_name = 'services',
   $api_endpoint = 'http://127.0.0.1:6385/v1',
-  $reserved_host_memory = '0',
+  $reserved_host_memory = '0'
 ) {
 
   include nova::params
 
   nova_config {
-    'IRONIC/admin_username':            value => $admin_user
-    'IRONIC/admin_password':            value => $admin_passwd
-    'IRONIC/admin_url':                 value => $admin_url
-    'IRONIC/admin_tenant_name':         value => $admin_tenant_name
-    'IRONIC/api_endpoint':              value => $api_endpoint
-    'DEFAULT/reserved_host_memory_mb ': value => $reserved_host_memory
+    'IRONIC/admin_username':            value => $admin_user;
+    'IRONIC/admin_password':            value => $admin_passwd;
+    'IRONIC/admin_url':                 value => $admin_url;
+    'IRONIC/admin_tenant_name':         value => $admin_tenant_name;
+    'IRONIC/api_endpoint':              value => $api_endpoint;
+    'DEFAULT/reserved_host_memory_mb ': value => $reserved_host_memory;
     'DEFAULT/compute_driver':           value => 'nova.virt.ironic.IronicDriver';
     'DEFAULT/compute_manager':          value => 'ironic.nova.compute.manager.ClusteredComputeManager';
   }
